0407013f4e4b3742a12c3fb6b4157a53930cfbd9
yescine <bouchoucha.yassine@gmail.com>
2020-10-26 19:53:45 +0100

    handle formik actions

--- src/components/Youtube.jsx
+++ src/components/Youtube.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
-
+import {useFormik} from 'formik';
 function Youtube () {
+  const formik = useFormik({
+    initialValues:{
+      name:'best',
+      email:'last@last',
+      channel:'none'
+    },
+    onSubmit:(values)=>{
+      console.log(values);
+    }
+  });
   return (
     <div>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <label htmlFor='name'>name</label>
-        <input type="text" id="name" name="name" />
+        <input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name} />
         <label htmlFor='email'>email</label>
-        <input type="email" id="email" name="email" />
+        <input type="email" id="email" name="email" onChange={formik.handleChange} value={formik.values.email} />
         <label htmlFor='channel'>channel</label>
-        <input type="text" id="channel" name="channel" />
+        <input type="text" id="channel" name="channel" onChange={formik.handleChange} value={formik.values.channel} />
 
         <button>Submit</button>
       </form>
